4d6bc05b1daa3f6996023368aebf8fd37cbdb34d
egage <egage@netopie.fr>
2023-10-09 18:59:51 +0200

    added controller, uibutton, input for cities,UIImage

---
+++ lebaluchon/Controller/WeatherViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WeatherViewController.swift
+//  lebaluchon
+//
+//  Created by Elodie Gage on 29/09/2023.
+//
+
+
+
+import Foundation
+import UIKit
+
+class WeatherViewController : UIViewController {
+    // outlets
+    
+    
+    @IBOutlet weak var weatherInformationDestination: UITextView!
+   
+    @IBOutlet weak var cityDestinationChoiceInput: UITextField!
+    
+    @IBOutlet weak var weatherImageInformationForeign: UIImageView!
+    
+    @IBOutlet weak var weatherImageInformationHome: UIImageView!
+    
+    @IBOutlet weak var cityHomeChoice: UITextField!
+        
+    @IBOutlet weak var weatherInformationHome: UITextView!
+    
+    @IBOutlet weak var saveLocationAsHomeButton: UIButton!
+    
+    
+
+    // call the func here
+    
+    
+    
+}
