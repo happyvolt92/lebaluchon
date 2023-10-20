8b77e45a3729fb99e131dd71d1d7b9f8fb145879
egage <egage@netopie.fr>
2023-10-20 17:42:04 +0200

    temperature bug fixed, it was in Calvin now working in Celsius

--- lebaluchon/Model/WeatherNews/WeatherServicesFile.swift
+++ lebaluchon/Model/WeatherNews/WeatherServicesFile.swift
@@ -7,33 +7,37 @@ class WeatherServices {
     private var session: URLSession
     // API key for OpenWeatherMap (Replace with your API key)
     private let apiKey = "af8da5af247d50350513bd332859d43c"
-
+    
     // Initialize with a custom URLSession or use the default configuration
     init(session: URLSession = URLSession(configuration: .default)) {
         self.session = session
     }
-
+    
     func fetchWeather(for city: String, completion: @escaping (Result<WeatherResponse, AppError>) -> Void) {
+        // Ensure the city name is properly encoded for the URL
         guard let encodedCity = city.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
             completion(.failure(.requestError))
             return
         }
-
-        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(encodedCity)&appid=\(apiKey)")!
-
+        
+        // Create the URL for the weather API request with the units=metric parameter
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(encodedCity)&appid=\(apiKey)&units=metric")!
+        
+        // Perform a data task to fetch weather data
         let task = session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(.requestError))
                 print("Error: \(error.localizedDescription)")
                 return
             }
-
+            
             guard let data = data else {
                 completion(.failure(.noDataAvailable))
                 return
             }
-
+            
             do {
+                // Decode the weather response from JSON
                 let weatherResponse = try JSONDecoder().decode(WeatherResponse.self, from: data)
                 completion(.success(weatherResponse))
             } catch {
@@ -41,7 +45,8 @@ class WeatherServices {
                 print("Parsing error: \(error.localizedDescription)")
             }
         }
-
+        
+        // Start the data task
         task.resume()
     }
 }
