67521bac05dc52908b7e02576911a8cb07c72835
egage <egage@netopie.fr>
2023-10-27 17:42:07 +0200

    currency response

---
+++ lebaluchon/Model/Currency/CurrencyResponse.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencyResponse.swift
+//  lebaluchon
+//
+//  Created by Elodie Gage on 27/10/2023.
+//
+import Foundation
+
+struct CurrencyResponse: Codable {
+    var date: String
+    var rates: Rate
+}
+
+struct Rate: Codable {
+    var USD: Double
+}
