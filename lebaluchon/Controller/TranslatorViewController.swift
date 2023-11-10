a04e39b803fdae58f7e0d02f0c55fb17c8512b52
egage <egage@netopie.fr>
2023-11-10 11:45:29 +0100

    clean plus cîmmentairetranslator

--- lebaluchon/Controller/TranslatorViewController.swift
+++ lebaluchon/Controller/TranslatorViewController.swift
@@ -1,53 +1,61 @@
 import UIKit
 
-class TranslatorViewController: UIViewController, UITextViewDelegate{
-    
+class TranslatorViewController: UIViewController, UITextViewDelegate {
+    // Outlets
     @IBOutlet var TranslatorView: UIView!
     @IBOutlet weak var TextViewToTranslate: UITextView!
     @IBOutlet weak var ToggleLanguages: UISegmentedControl!
     @IBOutlet weak var TranslateButton: UIButton!
     @IBOutlet weak var TextDestinationViewTranslated: UITextView!
     @IBOutlet weak var translatorActivityIndicator: UIActivityIndicatorView!
-    
+
     private var translatedText = ""
-    
-    
-    
+
+    // Action when the translation button is toggled
     @IBAction func toggleTranslationButton(_ sender: UIButton) {
+        // Check if the text view to translate is not empty
         guard !TextViewToTranslate.text.isEmpty else {
-            textViewAlert()
+            showAlert(for: .apiError) // Show an alert if there's no text to translate
             return
         }
-        // Start the activity indicator animation
+
+        // Start the activity indicator animation and initiate the translation
         ActivityIndicatorAnimation.shared.startLoading(for: translatorActivityIndicator)
         translate()
     }
-    
-    @IBAction func dismissKeyboard(_ sender : Any){
+
+    // Action to dismiss the keyboard
+    @IBAction func dismissKeyboard(_ sender: Any) {
         TextViewToTranslate.resignFirstResponder()
     }
-    
+
+    // Function to perform the translation
     private func translate() {
+        // Determine the target language based on the selected segment in the language toggle
         guard let language = switchLanguage() else {
             return
         }
+
+        // Call the translation service
         TranslatorService.shared.getTextTranslation(textToTranslate: TextViewToTranslate.text, from: language) { result in
             DispatchQueue.main.async {
-                // Stop the activity indicator animation when the API call completes
+                // Stop the activity indicator animation when the translation is complete
+
                 ActivityIndicatorAnimation.shared.stopLoading(for: self.translatorActivityIndicator)
-                
+
                 switch result {
                 case .failure:
-                    self.errorAlert()
+                    self.showAlert(for: .apiError) // Show an alert for translation failure
                 case .success(let traductor):
+                    // Update the translated text and display it in the destination text view
                     self.translatedText = traductor.data.translations.first?.translatedText ?? ""
                     self.updateTextView()
                 }
             }
         }
     }
-    
-    
+
+    // Function to switch the target language based on the selected segment
     private func switchLanguage() -> LanguagesOptions? {
         switch ToggleLanguages.selectedSegmentIndex {
         case 0:
@@ -58,36 +66,22 @@ class TranslatorViewController: UIViewController, UITextViewDelegate{
             return nil
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Set the delegate for the text view
         TextViewToTranslate.delegate = self
     }
+
+    // Delegate method called when the text view begins editing
     func textViewDidBeginEditing(_ textView: UITextView) {
         textView.becomeFirstResponder()
     }
-    
+
+    // Function to update the destination text view with the translated text
     private func updateTextView() {
         DispatchQueue.main.async {
-            // Update the text view with the translated text
             self.TextDestinationViewTranslated.text = self.translatedText
         }
     }
-    
-    private func errorAlert() {
-        let alert = UIAlertController(title: "Error", message: "Internet connection lost 🤔", preferredStyle: .alert)
-        let actionAlert = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        alert.addAction(actionAlert)
-        present(alert, animated: true, completion: nil)
-    }
-    
-    private func textViewAlert() {
-        let alert = UIAlertController(title: "Error", message: "Nothing + Nothing = Nothing! 😱 Add some text to see a translation", preferredStyle: .alert)
-        let actionAlert = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        alert.addAction(actionAlert)
-        present(alert, animated: true, completion: nil)
-    }
-    
 }
-
-    
